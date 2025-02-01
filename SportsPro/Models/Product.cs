0f2c757456843e0812f65fff8e1d2c10a48538ad
ASebring0715 <amays1116@gmail.com>
2025-02-01 16:24:09 -0600

    update 3

--- SportsPro/Models/Product.cs
+++ SportsPro/Models/Product.cs
@@ -1,13 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-
+using System.ComponentModel.DataAnnotations;
 namespace SportsPro.Models
 {
+	
     public class Product
     {
 		public int ProductID { get; set; }
+		[Required(ErrorMessage = "Please enter a Product Code.")]
 		public string ProductCode { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter a Product Name.")]
 		public string Name { get; set; } = string.Empty;
-		[Column(TypeName = "decimal(8,2)")]
+        [Required(ErrorMessage = "Please enter a Price.")]
+        [Column(TypeName = "decimal(8,2)")]
 		public decimal YearlyPrice { get; set; }
 		public DateTime ReleaseDate { get; set; } = DateTime.Now;
 	}
